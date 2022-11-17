d19de5dfea321945ca2578f1cddb2b0b5b933f11
Karen Shatveryan <107338482+karabass92@users.noreply.github.com>
2022-11-17 15:39:43 +0600

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,18 @@
+'''
+Find the missing letter
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+'''
+
+def find_missing_letter(chars):
+    dict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    result = dict[dict.index(chars[0]):dict.index(chars[-1]) + 1]
+    return ''.join([i for i in result if i not in chars])
